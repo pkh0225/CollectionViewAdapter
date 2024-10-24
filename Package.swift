eac85bff6725b81272dc8fb03b7538d0f9910ea1
pkh0225 <pkh0225@ssg.com>
2024-10-24 15:18:52 +0900

    spm

--- Package.swift
+++ Package.swift
@@ -11,19 +11,11 @@ let package = Package(
             name: "CollectionViewAdapter",
             targets: ["CollectionViewAdapter"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/pkh0225/EasyConstraints.git", from: "1.0.5"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CollectionViewAdapter",
-            dependencies: [
-//                .product(name: "EasyConstraints", package: "EasyConstraints")
-            ]),
+            name: "CollectionViewAdapter"),
         .testTarget(
             name: "CollectionViewAdapterTests",
             dependencies: ["CollectionViewAdapter"]),
