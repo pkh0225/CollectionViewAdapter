defd9923ae953390ad5948195d7f5add169a59a8
pkh0225 <pkh0225@ssg.com>
2024-10-25 11:01:14 +0900

    .
    .

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,26 +6,16 @@ import PackageDescription
 let package = Package(
     name: "CollectionViewAdapter",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CollectionViewAdapter",
             targets: ["CollectionViewAdapter"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pkh0225/EasyConstraints.git", from: "1.0.5"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CollectionViewAdapter",
-            dependencies: [
-                .product(name: "EasyConstraints", package: "EasyConstraints")
-            ]),
-        .testTarget(
-            name: "CollectionViewAdapterTests",
-            dependencies: ["CollectionViewAdapter"]),
+            name: "CollectionViewAdapter"),
+
     ]
 )
